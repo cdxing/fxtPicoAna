30cad83b2c5274fb3e5b8f0466fed3c82ad462ea
cdxing <danielxchenx@gmail.com>
2020-02-26 21:35:15 -0500

    EpdHit

--- PicoDstAnalyzer3.C
+++ PicoDstAnalyzer3.C
@@ -191,11 +191,11 @@ void PicoDstAnalyzer3(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPico
   // mEpFinder->SetRingWeights(2, double* RingWeights);    // RingWeights is a 1D array of 16 elements.
   TClonesArray * mEpdHits = new TClonesArray("StPicoEpdHit");
   unsigned int found;
-  // Retrieve picoDst
+  // Retrieve picoDst TChain*
   TChain *mPicoDst = picoReader->chain();
-  mPicoDst->SetBranchStatus("epdHit*",1,&found);   // note you need the asterisk
+  mPicoDst->SetBranchStatus("EpdHit*",1,&found);   // note you need the asterisk
   std::cout << "EpdHit Branch returned found= " << found << endl;
-  mPicoDst->SetBranchAddress("epdHit",&mEpdHits);
+  mPicoDst->SetBranchAddress("EpdHit",&mEpdHits);
 
   outFile.Append(".picoDst.result.root");
   // outFile.Prepend(Form("%d_%d_%d_",cutID,variationID,versionID));
