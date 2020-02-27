4ebeca8520e2b14f74e7d843d13f8b282ca2593c
cdxing <danielxchenx@gmail.com>
2020-02-26 22:32:39 -0500

    debug

--- PicoDstAnalyzer3.C
+++ PicoDstAnalyzer3.C
@@ -182,7 +182,7 @@ void PicoDstAnalyzer3(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPico
   std::cout << "Number of events to read: " << events2read << std::endl;
 
   // EPD EP finder to get EPD event plane
-  StEpdEpFinder *mEpFinder = new StEpdEpFinder(1,"epdTest"/*,*/);
+  StEpdEpFinder *mEpFinder = new StEpdEpFinder();
   int format = 2;
   mEpFinder->SetEpdHitFormat(format);    // format=0/1/2 for StEpdHit/StMuEpdHit/StPicoEpdHit
   mEpFinder->SetnMipThreshold(0.3);    // recommended by EPD group
